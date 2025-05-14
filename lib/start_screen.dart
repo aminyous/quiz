2a84e26925b4de56fa098c6bb9bf9eee07ee9bcc
Amine Hichri <amyn.hichri@gmail.com>
2025-05-14 14:03:38 +0100

    Add Start screen Page

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-
   const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Start Screen"));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 200,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Learn Flutter the fun way!",
+            style: TextStyle(
+              color: Color.fromARGB(255, 219, 213, 213),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.zero, // Square corners
+            ),
+            
+           foregroundColor: Colors.white
+          ),
+          child: const Text('Start Quiz'),
+        )
+        ],
+      ),
+    );
   }
 }
