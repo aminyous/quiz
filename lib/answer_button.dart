416681e1caf1f958ed2bebfcdcf7e40feef2ee72
Amine Hichri <amyn.hichri@gmail.com>
2025-07-16 12:00:22 +0200

    Add google font - Install and use a third party package

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  const AnswerButton({super.key, required this.answerText, required this.onTap });
+  const AnswerButton(
+      {super.key, required this.answerText, required this.onTap});
 
   final String answerText;
   final void Function() onTap;
@@ -15,13 +16,15 @@ class AnswerButton extends StatelessWidget {
             horizontal: 30,
             vertical: 10,
           ),
-          
           backgroundColor: const Color.fromARGB(255, 33, 1, 95),
           foregroundColor: Colors.white,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(40.0),
           ),
         ),
-        child: Text(answerText));
+        child: Text(
+          answerText,
+          textAlign: TextAlign.center,
+        ));
   }
 }
